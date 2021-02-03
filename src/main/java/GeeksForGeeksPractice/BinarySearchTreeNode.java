2ca316357e24dbba97b8e2fbd99c1082e1576aa8
Prasanna Mondhe <prasannamondhe@gmail.com>
2021-02-03 21:21:52 +0530

    Binary Search Tree Node class

--- src/main/java/GeeksForGeeksPractice/BinarySearchTreeNode.java
+++ src/main/java/GeeksForGeeksPractice/BinarySearchTreeNode.java
@@ -1,5 +1,7 @@
 package GeeksForGeeksPractice;
 
+import Search_Algorithms.BinarySearch;
+
 public class BinarySearchTreeNode {
     BinarySearchTreeNode left, right;
     int data;
@@ -73,18 +75,100 @@ public class BinarySearchTreeNode {
         }
     }
 
+    public BinarySearchTreeNode deleteNode(BinarySearchTreeNode node, int key){
+        BinarySearchTreeNode parent = null;
+        if(node == null){
+            return null;
+        }
+        if(key < node.data){
+            parent = node;
+            node.left = deleteNode(node.left, key);
+        }else if(key > node.data){
+            parent = node;
+            node.right = deleteNode(node.right, key);
+        }else{
+            if(node.right == null || node.left == null) {
+                BinarySearchTreeNode temp = null;
+                temp = node.left == null ? node.right : node.left;
+                if(temp == null){
+                    return null;
+                }else {
+                    return temp;
+                }
+            }else{
+                BinarySearchTreeNode successor = getSuccessor(node);
+                node.data = successor.data;
+                node.right = deleteNode(node.right, successor.data);
+                return node;
+            }
+
+        }
+        return node;
+    }
+
+    private BinarySearchTreeNode getSuccessor(BinarySearchTreeNode node) {
+        if(node == null){
+            return null;
+        }
+        BinarySearchTreeNode temp = node.right;
+        while (temp.left != null){
+            temp = temp.left;
+        }
+        return temp;
+    }
+
+    public void printRightTree(BinarySearchTreeNode node){
+        if(node == null){
+            System.out.println("Can't print the right tree");
+        }
+        printRightSubTree(node.right);
+    }
+
+    private void printRightSubTree(BinarySearchTreeNode node) {
+        if(node.left != null){
+            printRightSubTree(node.left);
+        }
+        System.out.println(node.data);
+        if(node.right != null){
+            printRightSubTree(node.right);
+        }
+    }
+
+    public int depthOfNode(BinarySearchTreeNode root, int key){
+        int depth =0;
+        if(root == null){
+            return -1;
+        }else{
+            if(key == root.data){
+                return depth;
+            }else
+        }
+    }
+
     public static void main(String[] args){
-        BinarySearchTreeNode treeNode = new BinarySearchTreeNode(3);
+        BinarySearchTreeNode treeNode = new BinarySearchTreeNode(8);
+        treeNode.insert(3);
+        treeNode.insert(10);
+        treeNode.insert(1);
+
+        treeNode.insert(6);
         treeNode.insert(4);
-        treeNode.insert(2);
+        treeNode.insert(7);
+        treeNode.insert(14);
+        treeNode.insert(13);
         System.out.println("In order is :");
         treeNode.printInOrder();
         System.out.println("Pre order is ");
         treeNode.printPreOrder();
         System.out.println("Post order is ");
         treeNode.printPostOrder();
-        System.out.println("Search element 4 "+treeNode.find(4));
+        //System.out.println("Search element 4 "+treeNode.find(4));
+        System.out.println("Delete node 3 : "+treeNode.deleteNode(treeNode, 3));
+        System.out.println("Inorder after node 3 is deleted ");
+        treeNode.printInOrder();
 
+        System.out.println("Print right side of BST");
+        treeNode.printRightTree(treeNode);
 
 
     }
